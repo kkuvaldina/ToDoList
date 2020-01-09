07217bd9f66ecc5af286ddd692a63a995db29005
Kseniia <47961615+kkuvaldina@users.noreply.github.com>
2020-01-08 18:36:18 -0700

    54

--- src/components/App.js
+++ src/components/App.js
@@ -6,25 +6,66 @@ import uuid from "uuid";
 class App extends React.Component {
   state = { tasks: [] };
 
-  onTaskSubmit = newTask => {
-    this.setState({
-      tasks: [...this.state.tasks, newTask]
-    });
-  };
-
-  handleDelete = task => {
-    const tasks = this.state.tasks.filter(k => k !== task);
-
+  handleSubmit = newTask => {
+    const tasks = [
+      ...this.state.tasks,
+      { text: newTask, id: `${uuid.v4()}`, taskCompleted: false }
+    ];
     this.setState({ tasks });
   };
 
-  
+  handleDelete = id => {
+    const tasks = this.state.tasks.filter(task => task.id !== id);
+    this.setState({ tasks });
+  };
+
+  handleComplete = id => {
+    const tasks = this.state.tasks.map(task => {
+      if (task.id === id) {
+        task.taskCompleted = true;
+        return task;
+      }
+      return task;
+    });
+    this.setState({ tasks });
+  };
+
+  getBadgeClasses() {
+    let classes = "";
+
+    if (this.state.taskCompleted === true) {
+      classes = "check";
+    }
+    return classes;
+  }
+
+  getTextStyle() {
+    let style = {
+      textDecorationLine: "line-through",
+      textDecorationStyle: "solid",
+      textSize: "1.7rem"
+    };
+    if (this.state.taskCompleted === true) {
+      return style;
+    }
+    return (style = { textSize: "1.7rem" });
+  }
+
   render() {
+    console.log(this.state.tasks);
     return (
       <div className="ui center aligned container">
-        <ToDoForm onFormSubmit={this.onTaskSubmit} />
-        {this.state.tasks.map((task, index) => (
-          <ToDoItem task={task} key={uuid.v4()} onDelete={this.handleDelete} />
+        <ToDoForm onFormSubmit={this.handleSubmit} />
+        {this.state.tasks.map(task => (
+          <ToDoItem
+            task={task.text}
+            key={task.id}
+            id={task.id}
+            onDelete={this.handleDelete}
+            onComplete={this.handleComplete}
+            getBadgeClasses={this.getBadgeClasses}
+            getTextStyle={this.getTextStyle}
+          />
         ))}
       </div>
     );
