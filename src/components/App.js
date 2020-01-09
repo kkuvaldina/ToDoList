f1b46cdf0d29b31fc3685d876ae207490d1cf069
Kseniia <kuvaldina.ks@gmail.com>
2020-01-08 20:58:01 -0700

    3

--- src/components/App.js
+++ src/components/App.js
@@ -8,48 +8,35 @@ class App extends React.Component {
 
   handleSubmit = newTask => {
     const tasks = [
-      ...this.state.tasks,
-      { text: newTask, id: `${uuid.v4()}`, taskCompleted: false }
+      { text: newTask, id: `${uuid.v4()}`, taskCompleted: false }, ...this.state.tasks
     ];
+    tasks.sort((a, b) => a.taskCompleted - b.taskCompleted);
     this.setState({ tasks });
   };
 
   handleDelete = id => {
     const tasks = this.state.tasks.filter(task => task.id !== id);
+    tasks.sort((a, b) => a.taskCompleted - b.taskCompleted);
     this.setState({ tasks });
   };
 
   handleComplete = id => {
     const tasks = this.state.tasks.map(task => {
       if (task.id === id) {
-        task.taskCompleted = true;
+        task.taskCompleted = !task.taskCompleted;
         return task;
       }
       return task;
     });
+    
+    tasks.sort((a, b) => a.taskCompleted - b.taskCompleted);
+
     this.setState({ tasks });
+
   };
 
-  getBadgeClasses() {
-    let classes = "";
 
-    if (this.state.taskCompleted === true) {
-      classes = "check";
-    }
-    return classes;
-  }
-
-  getTextStyle() {
-    let style = {
-      textDecorationLine: "line-through",
-      textDecorationStyle: "solid",
-      textSize: "1.7rem"
-    };
-    if (this.state.taskCompleted === true) {
-      return style;
-    }
-    return (style = { textSize: "1.7rem" });
-  }
+  
 
   render() {
     console.log(this.state.tasks);
@@ -58,13 +45,11 @@ class App extends React.Component {
         <ToDoForm onFormSubmit={this.handleSubmit} />
         {this.state.tasks.map(task => (
           <ToDoItem
-            task={task.text}
+            task={task}
             key={task.id}
-            id={task.id}
             onDelete={this.handleDelete}
             onComplete={this.handleComplete}
-            getBadgeClasses={this.getBadgeClasses}
-            getTextStyle={this.getTextStyle}
+            
           />
         ))}
       </div>
--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,22 +1,39 @@
 import React from "react";
 
 class ToDoItem extends React.Component {
+  getBadgeClasses = () => this.props.task.taskCompleted ? "check" : "";
+
+  getTextStyle = () => {
+    let style = {
+      textDecorationLine: "line-through",
+      textDecorationStyle: "solid",
+      fontSize: "1.7rem"
+    };
+    if (this.props.task.taskCompleted === true) {
+      return style;
+    }
+    return { fontSize: "1.7rem" };
+  }
+
   render() {
+    const task = this.props.task;
     return (
       <div
         className="ui raised left aligned clearing segment"
-        onClick={() => this.props.onComplete(this.props.id)}
+        onClick={() => this.props.onComplete(task.id)}
       >
         <i
-          className={`big circle outline icon ${() =>
-            this.props.getBadgeClasses()}`}
+          className={`big circle outline icon ${this.getBadgeClasses()}`}
         />
-        <span /*style={() => this.props.getTextStyle()}*/>
-          {this.props.task}
+        <span style={this.getTextStyle()}>
+          {task.text}
         </span>
         <button
           className="ui right floated button"
-          onClick={() => this.props.onDelete(this.props.id)}
+          onClick={(e) => {
+            e.stopPropagation();
+            this.props.onDelete(task.id)
+          }}
         >
           Delete
         </button>
