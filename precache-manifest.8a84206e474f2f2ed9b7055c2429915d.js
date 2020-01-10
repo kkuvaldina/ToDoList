339bf6e978c16f952c8dbe39deeab60283be8df1
Kseniia <kuvaldina.ks@gmail.com>
2020-01-09 20:15:20 -0700

    Updates

---
+++ precache-manifest.8a84206e474f2f2ed9b7055c2429915d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4721f655ada7383b9a4318e8de8ef9b1",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "27bbadccab5ae91f5882",
+    "url": "/ToDoList/static/css/main.bb4c7981.chunk.css"
+  },
+  {
+    "revision": "e58ac67864336540a3b6",
+    "url": "/ToDoList/static/js/2.a5856b35.chunk.js"
+  },
+  {
+    "revision": "27bbadccab5ae91f5882",
+    "url": "/ToDoList/static/js/main.bd67e8c0.chunk.js"
+  },
+  {
+    "revision": "6a71c43a298729cf07dd",
+    "url": "/ToDoList/static/js/runtime~main.67fadf76.js"
+  }
+]);
